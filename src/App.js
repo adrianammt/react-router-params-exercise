eadfbea52d4b69440c5139354e53b145f445f8bf
adrianammt <amorat@gmx.de>
2021-07-29 11:03:21 +0200

    params exercise

--- src/App.js
+++ src/App.js
@@ -1,42 +1,35 @@
 import "./styles.css";
-import {
-  BrowserRouter as Router,
-  NavLink,
-  Switch,
-  Route
-} from "react-router-dom";
+import { NavLink, Switch, Route } from "react-router-dom";
 import SingleUserPage from "./pages/SingleUserPage";
 import UsersPage from "./pages/UsersPage";
 
 export default function App() {
   return (
-    <Router>
-      <div className="App">
-        <header>
-          <h1> Routing & Fetching </h1>
-          <nav>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/users">Users</NavLink>
-          </nav>
-        </header>
-        <main>
-          <Switch>
-            <Route path="/users/x?">
-              <SingleUserPage />
-            </Route>
-            <Route path="/users">
-              <UsersPage />
-            </Route>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path="*">
-              <h2>No match </h2>
-            </Route>
-          </Switch>
-        </main>
-      </div>
-    </Router>
+    <div className="App">
+      <header>
+        <h1> Routing & Fetching </h1>
+        <nav>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/users">Users</NavLink>
+        </nav>
+      </header>
+      <main>
+        <Switch>
+          <Route path="/users/:userId">
+            <SingleUserPage />
+          </Route>
+          <Route path="/users">
+            <UsersPage />
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="*">
+            <h2>No match </h2>
+          </Route>
+        </Switch>
+      </main>
+    </div>
   );
 }
 
--- src/pages/SingleUserPage.js
+++ src/pages/SingleUserPage.js
@@ -1,3 +1,31 @@
+import { data } from "browserslist";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 export default function SingleUserPage() {
-  return <div>Your markup here</div>;
+  const { userId } = useParams();
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    const url = `https://reqres.in/api/users/${userId}`;
+
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setUser(data);
+      });
+  }, [userId]);
+
+  function renderUser() {
+    const { data } = user;
+    console.log(user);
+    return (
+      <section>
+        <h2>{data?.email}</h2>
+        {/* <img src={avatar} alt="name" /> */}
+      </section>
+    );
+  }
+
+  return renderUser();
 }
